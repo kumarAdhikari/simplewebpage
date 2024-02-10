61ea7c03c5ab681aac35ba9b5e31fe0398146baf
kumarAdhikari <80222698+kumarAdhikari@users.noreply.github.com>
2024-02-10 14:22:27 -0600

    Update products.php

--- products.php
+++ products.php
@@ -4,8 +4,8 @@ $host="127.0.0.1";
 $port=3306;
 $socket="";
 $user="root";
-$password="Jhapanepal8@";
-$dbname="adhikari";
+$password="database ko password";
+$dbname="database ko name";
 
 $conn = new mysqli($host, $user, $password, $dbname, $port, $socket)
 or die ('Could not connect to the database server' . mysqli_connect_error());
